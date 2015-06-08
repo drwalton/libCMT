3d9599aadbb145af557d3a541a44dee1f8e373b3
Fufu Fang <fangfufu2003@gmail.com>
2015-06-08 15:18:46 +0100

    got rid off the debug stuff

--- CMT.cpp
+++ CMT.cpp
@@ -30,15 +30,15 @@ static Mat in_std;
 static Mat out_mean;
 static Mat out_std;
 
-double pred_normcdf(double x)
+inline float pred_normcdf(float x)
 {
     // constants
-    double a1 =  0.254829592;
-    double a2 = -0.284496736;
-    double a3 =  1.421413741;
-    double a4 = -1.453152027;
-    double a5 =  1.061405429;
-    double p  =  0.3275911;
+    float a1 =  0.254829592;
+    float a2 = -0.284496736;
+    float a3 =  1.421413741;
+    float a4 = -1.453152027;
+    float a5 =  1.061405429;
+    float p  =  0.3275911;
 
     // Save the sign of x
     int sign = 1;
@@ -47,8 +47,8 @@ double pred_normcdf(double x)
     x = fabs(x)/sqrt(2.0);
 
     // A&S formula 7.1.26
-    double t = 1.0/(1.0 + p*x);
-    double y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*exp(-x*x);
+    float t = 1.0/(1.0 + p*x);
+    float y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*exp(-x*x);
 
     return 0.5*(1.0 + sign*y);
 }
@@ -134,10 +134,10 @@ Mat pred(const Mat img)
             prob_mat_3.at<Vec3f>(i, j)[1] * prob_mat_3.at<Vec3f>(i, j)[2];
         }
     }
-    printf("prob_mat size: %d, %d\n", prob_mat.rows, prob_mat.cols);
-    imshow("BLAH", prob_mat);
-    std::cout << prob_mat.size() << endl;
-    waitKey(30);
+//     printf("prob_mat size: %d, %d\n", prob_mat.rows, prob_mat.cols);
+//     imshow("BLAH", prob_mat);
+//     std::cout << prob_mat.size() << endl;
+//     waitKey(30);
     return prob_mat;
 }
 
@@ -266,7 +266,7 @@ CMT::CMT()
 
 void CMT::initialise(cv::Mat im0, cv::Rect target_bb)
 {
-    cvNamedWindow("BLAH");
+//     cvNamedWindow("BLAH");
     cv::Point2f topleft = target_bb.tl();
     cv::Point2f bottomright = target_bb.br();
 
